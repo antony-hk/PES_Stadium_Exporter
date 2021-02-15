05dfa3cfdbbb921d97d3f450b03cd7d34c78cb07
MjTs140914 <42273198+MjTs140914@users.noreply.github.com>
2021-02-15 19:56:11 +0700

    v0.6.4b Release...

--- PES_Stadium_Exporter.py
+++ PES_Stadium_Exporter.py
@@ -47,7 +47,7 @@ bl_info = {
 	"name": "PES Stadium Exporter",
 	"description": "eFootbal PES2021 PES Stadium Exporter",
 	"author": "MjTs-140914 || the4chancup",
-	"version": (0, 6, 3),
+	"version": (0, 6, 4),
 	"blender": (2, 90, 0),
 	"location": "Under Scene Tab",
 	"warning": "This addon is still in development.",
@@ -58,7 +58,7 @@ bl_info = {
 
 (major, minor, build) = bpy.app.version
 icons_collections = {}
-myver="v0.6.3b"
+myver="v0.6.4b"
 
 AddonsPath = str()
 AddonsPath = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
